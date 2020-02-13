03b186dcaf5e38b94716f53938a9192c075f5dbd
carpediem103 <s0563654@htw-berlin.de>
2020-02-13 17:05:21 +0100

    htw mensa session

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,4 @@
-var mqtt =  require('mqtt');
+/*var mqtt =  require('mqtt');
 
 
 var client = mqtt.connect("mqtt://1640f7e3:183a633f80feec7e@broker.shiftr.io",
@@ -11,7 +11,7 @@ client.on('connect', function(){
 
     client.subscribe('/yolo');
     // client.unsubscribe('/example');
-  
+
     setInterval(function(){
       client.publish('/yolo', 'yeeet');
     }, 1000);
@@ -20,4 +20,19 @@ client.on('connect', function(){
 
 client.on('message', function(topic, message) {
     console.log('new message:', topic, message.toString());
-  });
+  });*/
+
+
+let couplesJSONObj = {"coupleNames":[]};
+
+function sumbitLoverNames(){
+  var nameLover1 = document.getElementById("LoverName1").value;
+  var nameLover2 = document.getElementById("LoverName2").value;
+  //herz code:0x03
+//  var coupleObj = JSON.parse(couplesJSON);
+  couplesJSONObj['coupleNames'].push({"name1":nameLover1, "name2":nameLover2});
+  couplesJSON = JSON.stringify(couplesJSONObj);
+  console.log(couplesJSON);
+
+  
+}
