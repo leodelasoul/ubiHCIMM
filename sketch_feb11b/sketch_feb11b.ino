0b9c26af78ae873e1b1d421db89cf2d3f4079016
unknown <l.ricker93@web.de>
2020-02-15 20:45:15 +0100

    added arduino code

--- sketch_feb11b/sketch_feb11b.ino
+++ sketch_feb11b/sketch_feb11b.ino
@@ -1,48 +1,66 @@
-#include <Bridge.h>
-#include <YunClient.h>
-#include <MQTTClient.h>
+// This example uses an ESP32 Development Board
+// to connect to shiftr.io.
+//
+// You can check on your device after a successful
+// connection here: https://shiftr.io/try.
+//
+// by Joël Gähwiler
+// https://github.com/256dpi/arduino-mqtt
 
-YunClient net;
+#include <WiFi.h>
+#include <MQTT.h>
+
+WiFiClient net;
 MQTTClient client;
 
 unsigned long lastMillis = 0;
 
-void setup() {
-  Bridge.begin();
-  Serial.begin(115200);
-  client.begin("broker.shiftr.io", net);
-  connect();
-
-}
-
 void connect() {
-  Serial.print("connecting...");
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
 
-  
+  Serial.print("\nconnecting...");
   while (!client.connect("lovebridgeIO", "1640f7e3", "183a633f80feec7e")) {
-    
-    
+    Serial.print(".");
+    delay(1000);
   }
 
   Serial.println("\nconnected!");
+
+  client.subscribe("/hello");
+  // client.unsubscribe("/hello");
 }
 
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+}
+
+void setup() {
+  Serial.begin(115200);
+   WiFi.begin("Mahboii", "meisterball"); 
+
+  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
+  // You need to set the IP address directly.
+  client.begin("broker.shiftr.io", net);
+  client.onMessage(messageReceived);
+
+  connect();
+}
 
 void loop() {
-  // put your main code here, to run repeatedly:
   client.loop();
+  delay(10);  // <- fixes some issues with WiFi stability
 
-  if(!client.connected()) {
+  if (!client.connected()) {
     connect();
   }
 
-
-}
-
-void messageReceived(String topic, String payload, char * bytes, unsigned int length) {
-  Serial.print("incoming: ");
-  Serial.print(topic);
-  Serial.print(" - ");
-  Serial.print(payload);
-  Serial.println();
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 1000) {
+    lastMillis = millis();
+    client.publish("/hello", "world");
+  }
 }
