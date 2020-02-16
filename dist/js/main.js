bea155f0ba64dfb6e2b76d2950ecdabbb002bc10
unknown <l.ricker93@web.de>
2020-02-16 20:55:47 +0100

    rdy to deploy

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,17 +6,16 @@ const cors = require("cors");
 const router = express.Router();
 var bodyParser = require('body-parser')
 
-// app.use(express.static(__dirname + './..'), router, cors());
+app.use(express.static(__dirname + './..'), router, cors());
 app.use(bodyParser.json(),cors());
 
-// app.use(bodyParser.urlencoded({ extended: true }));
 //start application server on port 8080
 app.listen(process.env.PORT || 8080, () => {
   console.log("The server started on port 8080");
 });
-// app.get('*', function (req, res) {
-//   res.sendFile('index.html', { root: __dirname + './..' });
-// });
+app.get('*', function (req, res) {
+  res.sendFile('index.html', { root: __dirname + './..' });
+});
 
 
 var client = mqtt.connect("mqtt://1640f7e3:183a633f80feec7e@broker.shiftr.io",
@@ -24,7 +23,7 @@ var client = mqtt.connect("mqtt://1640f7e3:183a633f80feec7e@broker.shiftr.io",
 );
 
 app.post('/notify', (req, res) => {
-  var stringToSend = req.body.coupleNames[0].name1 + "<3" + req.body.coupleNames[0].name2
+  var stringToSend = req.body.coupleNames[0].name1 + " <3 " + req.body.coupleNames[0].name2
   client.publish('/loversNames', stringToSend);
 
   return res.send('Received a POST HTTP method');
@@ -38,10 +37,6 @@ client.on('connect', function () {
 
 });
 
-
-// client.on('message', function(topic, message) {
-//     console.log('new message:', topic, message.toString());
-//   });
 
 
 
