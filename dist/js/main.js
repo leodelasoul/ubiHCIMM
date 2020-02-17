e78a88a8471c10b58d368b4eb278fcdbf082d500
carpediem103 <s0563654@htw-berlin.de>
2020-02-17 14:35:38 +0100

    egal

--- dist/js/main.js
+++ dist/js/main.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser')
 
 
 app.use(express.static(__dirname + './..'), router, cors);
-
+/*
 app.use(function(req, res, next){
   res.header("Access-Control-Allow-Origin", 'localhost:8080');
   res.header("Access-Control-Allow-Credentials", true);
@@ -17,8 +17,8 @@ app.use(function(req, res, next){
   next();
   /*res.header("Access-Control-Allow-Origin", "localhost:8080"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin  , X-Requested-With, Content-Type, Accept");
-  next();*/
-});
+  next();
+});*/
 app.use(bodyParser.json());
 
 
