3165c1f2d468170bcae3c6ec26fab3f74d51905c
carpediem103 <s0563654@htw-berlin.de>
2020-02-17 14:40:57 +0100

    egal

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,7 +17,7 @@ app.use(function(req, res, next){
   next();
   /*res.header("Access-Control-Allow-Origin", "localhost:8080"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin  , X-Requested-With, Content-Type, Accept");
-  next();
+  next();*/
 });
 app.use(bodyParser.json());
 
